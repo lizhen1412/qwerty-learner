2cbb85d6dbfa631b9af98ea14860f6c359bede5f
Lambert <lambert@gmail.com>
2025-07-24 21:50:37 +0800

    fix: update Docker image name in restart script

--- scripts/docker_restartbuild.sh
+++ scripts/docker_restartbuild.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 docker-compose down
-docker rmi $(docker images -q qwertylearner)
+docker rmi $(docker images -q qwerty-learner-qwertylearner)
 docker-compose up -d
 
 echo "Docker 容器已重启并重新构建"
